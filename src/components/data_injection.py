c8b69b0511d5d62d5dd4ade57d59a9acc9ecca09
sahilbadgujar <sahilbadgujar100@gmail.com>
2025-02-06 23:38:27 +0530

    final commit

--- src/components/data_injection.py
+++ src/components/data_injection.py
@@ -1,64 +1,60 @@
-''' In this components we will create a code which will able to read
- the data from any datasource 
- We also here divide data into training and testing purpose
- '''
-
-import os 
+import os
 import sys
-from src.exception_handling import CustomException
-from src.login import logging
+from src.exception import CustomException
+from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-
-# inside a class to define class variable you have to use __init__ but with decorator class which is here @dataclass
-#you can directly define the varaiable
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
-class DataInjectionConfig:
-    # when we train or test or raw data some data and if we want to see that data we can save it in a special folder that is done below
-    train_data_path: str=os.path.join('artifact',"train.csv")
-    test_data_path: str=os.path.join('artifact',"test.csv")
-    raw_data_path: str=os.path.join('artifact',"data.csv")
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
-class DataInjection:
+class DataIngestion:
     def __init__(self):
-        self.injection_config = DataInjectionConfig()
+        self.ingestion_config=DataIngestionConfig()
 
-    def initiate_data_injection(self):
-        logging.info("Entered the data injection or component")
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('notebook/data/stud.csv')
-            logging.info("Read the Dataset as dataframe")
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.injection_config.train_data_path),exist_ok=True) # it saves the file to desired path 
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-            df.to_csv(self.injection_config.raw_data_path,index=False,header=True) # save the raw data to this path
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
             logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
-            train_set, test_set = train_test_split(df, test_size=0.2, random_state = 24)
-            train_set.to_csv(self.injection_config.train_data_path,index=False,header=True) # save the train data to this path
-            test_set.to_csv(self.injection_config.test_data_path,index=False,header=True)  # save the test data to this path
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
-            logging.info("Injection of data is completed")
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            return{
-                self.injection_config.train_data_path,
-                self.injection_config.test_data_path
-            }
+            logging.info("Inmgestion of the data iss completed")
 
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
 
+            )
         except Exception as e:
             raise CustomException(e,sys)
         
-if __name__ == "__main__":
-    obj=DataInjection()
-    train_data, test_data = obj.initiate_data_injection()
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
-    data_tranformation = DataTransformation()
-    data_tranformation.initiate_data_transformation(train_data, test_data)
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
